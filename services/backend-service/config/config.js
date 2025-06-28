8ee1da25ebda2f9cf8dd9c9d7e1d656950ab55e7
rifushigi <rifushigi.teda@gmail.com>
2025-06-28 21:45:23 +0100

    feat: Add configuration file for database connection in development environment

---
+++ services/backend-service/config/config.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+module.exports = {
+    development: {
+        username: process.env.DB_USER || 'backend',
+        password: process.env.DB_PASSWORD || 'backendpass',
+        database: process.env.DB_NAME || 'zerotrust',
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 5432,
+        dialect: 'postgres',
+        logging: false
+    }
+}; 
