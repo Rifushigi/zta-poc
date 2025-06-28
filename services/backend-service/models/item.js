795bcfcad4da8adfc06e696528e9436e45b2b0d8
rifushigi <rifushigi.teda@gmail.com>
2025-06-28 21:46:02 +0100

    feat: Implement Item model with validation and associations

---
+++ services/backend-service/models/item.js
@@ -0,0 +1,51 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Item extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Item.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [1, 255]
+      }
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [1, 1000]
+      }
+    },
+    owner: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Item',
+    paranoid: true,
+    indexes: [
+      {
+        fields: ['owner']
+      }
+    ]
+  });
+  return Item;
+};
