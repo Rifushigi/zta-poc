8565be09a271e9af1d72d9f7f232d84fda01b41a
Rifushigi <rifushigi.teda@gmail.com>
2025-07-04 16:06:57 +0100

    refactor: always log to the console in the logger configuration for improved visibility

--- services/backend-service/utils/logger.js
+++ services/backend-service/utils/logger.js
@@ -20,15 +20,13 @@ const logger = winston.createLogger({
     ],
 });
 
-// If we're not in production, log to the console as well
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.combine(
-            winston.format.colorize(),
-            winston.format.simple()
-        )
-    }));
-}
+// Always log to the console, regardless of environment
+logger.add(new winston.transports.Console({
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+    )
+}));
 
 // Create a stream object for Morgan
 logger.stream = {
