8b1573320ca7f582ba4c7c250493d08a570abe0c
Rifushigi <rifushigi.teda@gmail.com>
2025-07-04 19:49:48 +0100

    feat: implement Sequelize database connection and initialize models in index.js

---
+++ services/backend-service/models/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.Item = require('./item')(sequelize, Sequelize.DataTypes);
+
+module.exports = db; 
